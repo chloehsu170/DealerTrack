d8e395ccc681fe862b51225143d4f0e90c8c1794
chloehsu170 <xuyanwen170@gmail.com>
2021-09-22 20:49:11 -0500

    main file renewal

--- main.py
+++ main.py
@@ -9,13 +9,13 @@ import os
 from common.HTMLTestRunner import HTMLTestRunner
 from common.dir_config import *
 
-s=unittest.TestSuite()
+ss=unittest.TestSuite()
 loader=unittest.TestLoader()
-s.addTests(loader.discover(testcases_dir))
+ss.addTests(loader.discover(testcases_dir))
 with open(htmlreport_dir+"\\autoTest_report.html",'wb') as fp:
     runner=HTMLTestRunner(
         stream=fp,
         title="Dealertarck's Test Report",
         description='Auto UI Test Coding Session',
     )
-    runner.run(s)
+    runner.run(ss)
