d07953f889250400787bf544a789350dce519361
“ <xuyanwen170@gmail.com>
2021-09-23 11:36:58 -0500

    0923 firstcommit

--- main.py
+++ main.py
@@ -11,7 +11,8 @@ from common.dir_config import *
 
 ss=unittest.TestSuite()
 loader=unittest.TestLoader()
-ss.addTests(loader.discover(testcases_dir))
+ss.addTests(loader.discover(testcases_dir))#add testcases into test suite
+# unittest.TestSuite().addTests(unittest.TestLoader().discover(testcases_dir))
 with open(htmlreport_dir+"\\autoTest_report.html",'wb') as fp:
     runner=HTMLTestRunner(
         stream=fp,
@@ -19,3 +20,17 @@ with open(htmlreport_dir+"\\autoTest_report.html",'wb') as fp:
         description='Auto UI Test Coding Session',
     )
     runner.run(ss)
+
+
+# ss=unittest.TestSuite()
+# loader=unittest.TestLoader()
+# ss.addTests(loader.discover(testcases_dir))
+# 
+# with open(htmlreport_dir+'\\autotest_report.html','wb') as fp:
+#     runner=HTMLTestRunner(
+#         stream=fp,
+#         verbosity=2, #print the error message for debug
+#         title="dfd",
+#         description="des",
+#     )
+#     runner.run(ss)
