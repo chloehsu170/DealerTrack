973ebc2313c6174fd69c3950e07b1ecdde9f36bd
“ <xuyanwen170@gmail.com>
2021-09-28 14:13:58 -0500

    0928

--- PageObjects/first_page.py
+++ PageObjects/first_page.py
@@ -11,75 +11,64 @@ from selenium.webdriver.support import expected_conditions as EC
 from PageLocators.first_page_locator import FirstPageLocator as loc
 from common.basepage import BasePage
 
+
 class FirstPage(BasePage):
     def check_user_ele_exists(self):
         '''
         description:check the page message
         '''
-        # WebDriverWait(self.driver,15).until(EC.visibility_of_element_located(loc.loginArea_loc))
-
-        # try:
-        #     self.driver.find_element(loc.loginArea_loc)
-        # except Exception as e:
-        #     return e
-        # else:
-        #     return self.get_element_text("check the login message", loc.loginArea_loc)
         self.wait_eleVisible(loc.loginArea_loc)
-        return self.get_element_text("check the page message",loc.loginArea_loc)
+        return self.get_element_text("check the page message", loc.loginArea_loc)
 
     def addition(self, first, second):
         '''
         description: input first number and second number of the addition 
         '''
         # waiting for the element
-        # WebDriverWait(self.driver,30,0.5).until(EC.visibility_of_element_located(loc.first_number_loc))
-        # print('element showd up:',*loc.first_number_loc)
         self.wait_eleVisible(loc.first_number_loc)
         # input text
-        self.input_text(first,"input first number in the first page",loc.first_number_loc)
+        self.input_text(
+            first, "input first number in the first page", loc.first_number_loc)
         # waiting for the element
         self.wait_eleVisible(loc.second_number_loc)
         # input text
-        self.input_text(second,"input second number in the first page",loc.second_number_loc)
-        self.click("click addition button",loc.add_button_loc)
+        self.input_text(
+            second, "input second number in the first page", loc.second_number_loc)
+        self.wait_eleVisible(loc.add_button_loc)
+        self.click("click addition button", loc.add_button_loc)
 
-    def login(self,username):
+    def login(self, username):
         '''
         description: check the login function
         '''
         # waiting for the element
         self.wait_eleVisible(loc.user_loc)
         # input text
-        self.input_text(username,"first page input username",loc.user_loc)
-        self.click("click login button",loc.login_button_loc)
-    
+        self.input_text(username, "first page input username", loc.user_loc)
+        self.wait_eleVisible(loc.login_button_loc)
+        self.click("click login button", loc.login_button_loc)
+
     def get_loginName(self):
         '''
         description: get login name
         '''
         self.wait_eleVisible(loc.loginArea_loc)
-        return self.get_element_text("get login name",loc.loginArea_loc)
-    
+        return self.get_element_text("get login name", loc.loginArea_loc)
+
     def get_resultOfAddition(self):
         '''
         description: get result of addition
         '''
         self.wait_eleVisible(loc.result_of_addition_loc)
-        return self.get_element_attr_value("get result of addition",loc.result_of_addition_loc)
-    
+        return self.get_element_attr_value("get result of addition", loc.result_of_addition_loc)
+
     def get_errorMsg(self):
         '''
         description: get login error message
         '''
-        #accept the alert info
-        try:
-            alert = self.driver.switch_to_alert()
-            message = alert.text
-            alert.accept()
-            return message
-        except:
-            pass
-
+        # accept the alert info
+        return self.switch_alert(10)
+        
     # def get_error_pageMsg(self):
     #     WebDriverWait(self.driver,30,0.1).until(EC.visibility_of_element_located(loc.pageCenter_loc))
     #     return self.driver.find_element(loc.pageCenter_loc).text
--- common/basepage.py
+++ common/basepage.py
@@ -13,65 +13,70 @@ from common import logger
 import time
 from common.dir_config import screenshot_dir
 
-class BasePage:
-    def __init__(self,driver:WebDriver):
-        self.driver=driver
-        self.mylog = logger.log()
 
-    def wait_eleVisible(self,loc):
+class BasePage:
+    def __init__(self, driver: WebDriver):
+        self.driver = driver
+        self.mylog = logger.Log()
+
+    def wait_eleVisible(self, loc):
         '''
         description: wait until the element is visible
         '''
         start = time.time()
         try:
-            # WebDriverWait(self.driver,30,0.5).until(EC.visibility_of_element_located(loc))
-            WebDriverWait(self.driver,30,0.5).until(EC.presence_of_element_located(loc))#visibility_of_element_located
-            print('visible or not')
+            WebDriverWait(self.driver, 30, 0.5).until(
+                EC.visibility_of_element_located(loc))  # presence_of_element_located
         except:
-            self.mylog.error("wait until the element {} is visible, but time is out！".format(loc))
+            self.mylog.error(
+                "wait for the element {} is visible, but time is out！".format(loc))
             raise
         else:
             end = time.time()
             duration = end-start
-            self.mylog.info("wait {} until the element {} is visible".format(duration,loc))
+            self.mylog.info(
+                "wait {} until the element {} is visible".format(duration, loc))
 
-    def wait_valueVisible(self,value,loc):
+
+    def wait_valueVisible(self, value, loc):
         '''
         description: wait until the element's value is visible
         '''
         start = time.time()
         try:
-            WebDriverWait(self.driver,30,0.5).until(EC.text_to_be_present_in_element_value(loc,value,))#visibility_of_element_located
+            WebDriverWait(self.driver, 30, 0.5).until(EC.text_to_be_present_in_element_value(
+                loc, value))  # visibility_of_element_located
         except:
-            self.mylog.error("wait until the element {}'s value is visible, but time is out！".format(loc))
+            self.mylog.error(
+                "wait until the element {}'s value is visible, but time is out！".format(loc))
             return False
         else:
             end = time.time()
             duration = end-start
-            self.mylog.info("wait {} until the element {}'s value is visible".format(duration,loc))
+            self.mylog.info(
+                "wait {} until the element {}'s value is visible".format(duration, loc))
             return True
-
-
-    def get_element(self,doc,loc):
+            
+    def get_element(self, doc, loc):
         '''
         description: find the element's locator
         '''
         try:
-            # print((223,loc),*loc)
-            return self.driver.find_element(*loc)
+            ele = self.driver.find_element(*loc)
         except:
             self.mylog.error("fail to find the element: {}".format(loc))
             self.save_img(doc)
             raise
         else:
-            self.mylog.info("succeed to find the element: {} of {}".format(doc,loc))
+            self.mylog.info(
+                "succeed to find the element: {} of {}".format(doc, loc))
+            return ele
 
-    def input_text(self,value,doc,loc):
+    def input_text(self, value, doc, loc):
         '''
         description: input the text into the element
         '''
-        self.wait_eleVisible(loc)
-        ele = self.get_element(doc,loc)
+        ele = self.get_element(doc, loc)
         try:
             ele.clear()
             if value == 'Keys.SPACE':
@@ -81,20 +86,21 @@ class BasePage:
             else:
                 ele.send_keys(value)
         except:
-            self.mylog.error("fail to input {} into the element: {}".format(value,loc))
+            self.mylog.error(
+                "fail to input {} into the element: {}".format(value, loc))
             self.save_img(doc)
             raise
         else:
-            self.mylog.info("succeed to input {} into the element: {}".format(value,loc))
+            self.mylog.info(
+                "succeed to input {} into the element: {}".format(value, loc))
 
-    def click(self,doc,loc):
+    def click(self, doc, loc):
         '''
         description: click the element
         '''
         self.wait_eleVisible(loc)
-        # ele=self.get_element(loc,doc)
         try:
-            self.get_element(doc,loc).click()
+            self.get_element(doc, loc).click()
         except:
             self.mylog.error("fail to click the element: {}".format(loc))
             self.save_img(doc)
@@ -102,34 +108,65 @@ class BasePage:
         else:
             self.mylog.info("succeed to click the element: {}".format(loc))
 
-    def get_element_text(self,doc,loc):
+    def get_element_text(self, doc, loc):
         '''
         description: get element's text
         '''
-        self.wait_eleVisible(loc)
-        # ele=self.get_element(loc,doc)
+        # self.wait_eleVisible(loc)
         try:
-            return self.get_element(doc,loc).text
+            text = self.get_element(doc, loc).text
         except:
-            self.mylog.error("fail to click the element: {}".format(loc))
+            self.mylog.error("fail to get element text: {}".format(loc))
             self.save_img(doc)
             raise
         else:
-            self.mylog.info("succeed to click the element: {}".format(loc))
+            self.mylog.info(
+                "succeed to get element {}'s text: {}".format(loc, text))
+            return text
 
-    def get_element_attr_value(self,doc,loc):
+
+    def get_element_attr_value(self, doc, loc):
         '''
         description: get element's attribute value
         '''
-        self.wait_eleVisible(loc)
+        # self.wait_eleVisible(loc)
         try:
-            return self.get_element(doc,loc).get_attribute('value')
+            value = self.get_element(doc, loc).get_attribute('value')
         except:
-            self.mylog.error("fail to click the element: {}".format(loc))
+            self.mylog.error("fail to get element attr value: {}".format(loc))
             self.save_img(doc)
             raise
         else:
-            self.mylog.info("succeed to click the element: {}".format(loc))
+            self.mylog.info(
+                "succeed to get element {}'s attr value: {}".format(loc, value))
+            return value
+
+    def save_img(self, doc=""):
+        '''
+        description: save the image of the screenshot
+        '''
+        cur_time = time.strftime(r"%Y-%m-%d",time.localtime())
+        file = screenshot_dir+"/{}_{}.png".format(doc, cur_time)
+        try:
+            self.driver.save_screenshot(file)
+        except:
+            self.mylog.error("fail to take a screenshot")
+        else:
+            self.mylog.info(
+                "succeed to take a screenshot which is saved in the path: {}".format(file))
+
+    def switch_alert(self,timeout):
+        try:
+            WebDriverWait(self.driver,timeout).until(EC.alert_is_present())
+            alert = self.driver.switch_to_alert()
+        except:
+            self.mylog.error("fail to switch to alert, time is out!")
+        else:
+            self.mylog.info("succeed to switch to alert")
+            message = alert.text
+            alert.accept()
+            return message
+
 
     # def switch_frame(self,locator,timeout):
     #     try:
@@ -144,16 +181,3 @@ class BasePage:
     #         pass
     #     else:
     #         pass
-
-    def save_img(self,doc=""):
-        '''
-        description: save the image of the screenshot
-        '''
-        cur_time = ""
-        file = screenshot_dir+"/{}_{}.png".format(doc,cur_time)
-        try:
-            self.driver.save_screenshot(file)
-        except:
-            self.mylog.error("fail to take a screenshot")
-        else:
-            self.mylog.info("succeed to take a screenshot which is saved in the path: {}".format(file))
