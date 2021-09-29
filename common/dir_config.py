160aa4db6a5a20de6a8ac73b63c0a7155b584748
chloehsu170 <xuyanwen170@gmail.com>
2021-09-28 22:47:34 -0500

    mac path

--- common/dir_config.py
+++ common/dir_config.py
@@ -1,9 +1,9 @@
 # /!/usr/bin/python3
 # coding:utf-8
-__author__ = 'Yanwen Xu'
-'''
+__author__ = "Yanwen Xu"
+"""
 description:Dealertarck's dir configuration
-'''
+"""
 
 import os
 import sys
@@ -11,11 +11,14 @@ import sys
 syspath = sys.path
 base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # base_dir=os.path.split(os.path.split(os.path.abspath(__file__))[0])[0]
-# sys.path.append(base_dir)
-# print(syspath)
-
+# sys.path.app
+# end(base_dir)
+base_dir = base_dir.replace("\\", "/")
+syspath.append(base_dir)
+print("base_dir", syspath)
 testdatas_dir = os.path.join(base_dir, "TestDatas")
 testcases_dir = os.path.join(base_dir, "TestCases")
 htmlreport_dir = os.path.join(base_dir, "Outputs\\report")
 logs_dir = os.path.join(base_dir, "Outputs\\logs")
 screenshot_dir = os.path.join(base_dir, "Outputs\\screenshots")
+print("base_dir", testdatas_dir)
